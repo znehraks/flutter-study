0652e218a79364dc282946702f63d6a40178e1b8
znehraks <znehraks@gmail.com>
2023-02-14 14:18:29 +0900

    initialize

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/screens/home_screen.dart';
 
 void main() {
   runApp(App());
 }
 
 class App extends StatelessWidget {
-  App({super.key});
-  bool showTitle = true;
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        theme: ThemeData(
-          textTheme: const TextTheme(
-            displayLarge: TextStyle(
-              color: Color(0xFF232B55),
-            ),
-          ),
-          cardColor: const Color(0xFFF4EDDB),
-          colorScheme: ColorScheme.fromSwatch(
-            backgroundColor: const Color(
-              0xFFE7626C,
-            ),
-          ),
-        ),
-        home: const HomeScreen());
+    return const MaterialApp();
   }
 }
